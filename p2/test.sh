3b6f3979f79a61d36cf52a9da3cbb559284b7746
Ayoub Bouras <aybouras@e3r9p15.1337.ma>
2022-06-20 17:00:43 +0100

    part 3 loading...

--- p2/test.sh
+++ p2/test.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
 while (true)
-        do
-            output=$(curl -s -o /dev/null -w '%{http_code}' 192.168.42.110)
-            echo $output
-            if [ $output == "200" ]; then
-                echo -e "\033[0;32m HTTP request was successful .....\033[0m"
-                exit 0
-            else
-                echo -e "\033[0;31m HTTP server fails to deliver the page .....\033[0m"
-                sleep 2
-            fi
-        done
+    do
+        output=$(kubectl get pods --namespace inception | awk '{ print $3 }' | awk 'NR!=1' | grep Running | wc -l)
+         
+        if [ $output == "1" ]; then
+            echo -e "\033[0;32m Pod1 is Running .....\033[0m"
+     
+        elif [ $output == "2" ]; then
+            echo -e "\033[0;32m Pod2 is Running .....\033[0m"
+     
+        elif [ $output == "3" ]; then
+            echo -e "\033[0;32m Pod3 is Running .....\033[0m"
+     
+        elif [ $output == "4" ]; then
+            echo -e "\033[0;32m Pod4 is Running .....\033[0m"
+     
+        elif [ $output == "5" ]; then
+            echo -e "\033[0;32m \nAll Pods are Ready and  Running .....\n \033[0m"
+            sleep 2
+            # exit 0
+            break
+
+        else 
+            echo -e "\033[0;31m Waiting for Pods to get Ready .....\033[0m"
+            sleep 4
+        fi
+    
+    done
