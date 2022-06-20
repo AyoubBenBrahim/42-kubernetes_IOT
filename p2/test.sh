ddc53fa14f468a64424035a501083bd6e997f6dc
Ayoub Bouras <aybouras@e3r9p15.1337.ma>
2022-06-20 15:08:17 +0100

    part 3 loading...

--- p2/test.sh
+++ p2/test.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-   while (true)
+while (true)
         do
             output=$(curl -s -o /dev/null -w '%{http_code}' 192.168.42.110)
-             
-         if [ $output == "200" ]; then
-            echo -e "\033[0;32m HTTP request was successful .....\033[0m"
-            exit 0
-         
-         else
-            echo -e "\033[0;31m HTTP server fails to deliver the page .....\033[0m"
-            sleep 2
-         fi
-        
+            echo $output
+            if [ $output == "200" ]; then
+                echo -e "\033[0;32m HTTP request was successful .....\033[0m"
+                exit 0
+            else
+                echo -e "\033[0;31m HTTP server fails to deliver the page .....\033[0m"
+                sleep 2
+            fi
         done
