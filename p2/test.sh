1a8ccad6d6833072f2fd7fced023401f35fa5389
Ayoub Bouras <aybouras@e3r9p15.1337.ma>
2022-06-20 14:56:13 +0100

    part 3 loading...

--- p2/test.sh
+++ p2/test.sh
@@ -1,32 +1,16 @@
 #!/bin/sh
 
-#output=$(kubectl get pods --namespace default | awk '{ print $3 }' | awk 'NR!=1' | grep Running | wc -l)
-
-while (true)
-    do
-        output=$(kubectl get pods --namespace default | awk '{ print $3 }' | awk 'NR!=1' | grep Running | wc -l)
+   while (true)
+        do
+            output=$(curl -s -o /dev/null -w '%{http_code}' 192.168.42.110)
+             
+         if [ $output == "200" ]; then
+            echo -e "\033[0;32m HTTP request was successful .....\033[0m"
+            exit 0
          
-     if [ $output == "1" ]; then
-              echo -e "\033[0;32m Pod1 is Running .....\033[0m"
-     
-     elif [ $output == "2" ]; then
-            echo -e "\033[0;32m Pod2 is Running .....\033[0m"
-     
-     elif [ $output == "3" ]; then
-            echo -e "\033[0;32m Pod3 is Running .....\033[0m"
-     
-     elif [ $output == "4" ]; then
-            echo -e "\033[0;32m Pod4 is Running .....\033[0m"
-     
-     elif [ $output == "5" ]; then
-            echo -e "\033[0;32m All Pods are Ready and  Running .....\033[0m"
-        sleep 1
-        exit 0
-    
-     else 
-        echo -e "\033[0;32m Waiting for Pods to get Ready .....\033[0m"
-     fi
-    
-    done
-
-
+         else
+            echo -e "\033[0;31m HTTP server fails to deliver the page .....\033[0m"
+            sleep 2
+         fi
+        
+        done
