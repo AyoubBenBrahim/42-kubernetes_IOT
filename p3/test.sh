64b367cb1d6b609848249fe21908cd24c7cd8767
Ayoub Bouras <aybouras@e3r9p15.1337.ma>
2022-06-23 20:17:47 +0100

    p3 98%

---
+++ p3/test.sh
@@ -0,0 +1,31 @@
+#  while (true)
+#         do         
+#             # sleep 3
+        
+
+#             # output=$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}")
+#             # len=${#output}
+#             # if [ $len -eq 24 ]; then 
+#             #     echo "agroCD password is: "
+#             #     sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+#             #     exit 0
+#             # fi
+
+
+#         done
+
+  while (true)
+        do
+            output=$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}") > /dev/null 2>&1
+            len=${#output}
+            
+            if [ $len -eq 24 ]; then 
+                echo "agroCD password is: "
+                sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+                exit 0
+            fi
+
+            echo "\033[0;31m waiting for argoCD to get ready .....\033[0m"
+            sleep 4
+
+        done
